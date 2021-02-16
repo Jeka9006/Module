06bb84560fe52311f877051ebd3f16932f010f45
DESKTOP-9C62S0S\umn90 <umn90jeka@gmail.com>
2021-02-16 09:53:00 +0200

    добавление сортировки

--- Module/Module/Program.cs
+++ Module/Module/Program.cs
@@ -6,7 +6,39 @@ namespace Module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.Write("Введите элементы массива:\t");
+
+
+
+
+            int elementArrey = int.Parse(Console.ReadLine());
+
+           int[] A = new int[elementArrey];
+            int[] B = new int[elementArrey];
+
+            int[] numericArray = new int[elementArrey];
+
+            Random N = new Random();
+            for (int i = 0; i < numericArray.Length; i++)
+                numericArray[i] = N.Next(0, 27);
+
+            for (int i = 0; i < numericArray.Length; i++)
+            {
+                if (numericArray[i] % 2 == 0)
+                {
+                    A[i] += numericArray[i];
+                }
+
+                else
+                {
+                    B[i] += numericArray[i];      
+                }
+                
+                Console.WriteLine(A[i]);
+            }
+
+            
         }
+        
     }
-}
+    }
